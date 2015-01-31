1db1b963bf8cd2adfaa43b2ce626939abb0039ae
Max Hunt <scarymrmoley@hotmail.co.uk>
2015-01-31 22:32:35 +0000

    fuck, i hope this works

--- src/app.js
+++ src/app.js
@@ -20,17 +20,17 @@ var CountScreen = new UI.Window();
 //Elements for AccelerometerScreen
 var TitleText = new UI.Text({ position: new Vector2(0,0), size: new Vector2(144, 168) });
 var CounterText = new UI.Text({ position: new Vector2(0,25), size: new Vector2(144, 168) });
-
-//lower hertz Values
+//var gesture = [[{x:'',y:'',z:''},{x:'50',y:'200',z:'20',xpos:'true',xneg:'false',ypos:'true',yneg:'false',zpos:'true',zneg:'true'}]];//lower hertz Values
+var gesture = [[{x:'',y:'',z:''},{x:'50',y:'200',z:'20'},{x:'',y:'',z:''},{x:'50',y:'200',z:'20'}]];
 Accel.config({
    rate: 25,
    sample: 5,
    subscribe: false
 });
 
-var xAxis = 0;
-var yAxis = 0;
-var zAxis = 0;
+//var xAxis = 0;
+//var yAxis = 0;
+//var zAxis = 0;
 var counter = 0;
 
 var inWristCount = false;
@@ -59,13 +59,6 @@ function onClick(e) {
    CountScreen.on('click','back',onAccelBack);   
    CountScreen.on('accelData', onPeek);        
 }
-
-//Close Screen and Stop loop
-function onAccelSelect(e){   
-   console.log('Post Array');
-   console.log('Current data samples are: ' + e.samples); 
-}
-
 //Close Screen and Stop loop
 function onAccelBack(){
    console.log('Close Screen and Stop Loop');
@@ -77,10 +70,10 @@ function onPeek(e){
    console.log('Accel data: ' + JSON.stringify(e.accels));
    if (inWristCount === true){
       console.log('Peeking'); 
-      xAxis = e.accel.x;
-      yAxis = e.accel.y;
-      zAxis = e.accel.z;
-      insertElements();       
+      var frameArray = [];
+      frameArray= arrayToFrames(e);
+      var detection = detectGesture(e,frameArray);
+      insertElements(detection);       
    }
    else{
       console.log("emptyfunction");
@@ -91,13 +84,41 @@ function onPeek(e){
    
    }      
 }
-
-//
-function detectWristRoll(){
-      //placeholder
+function arrayToFrames(e){
+   var frameArray = [];
+	for ( var i=0; i<e.accels.length-1; i++){
+		frameArray.push([e.accels[i],e.accels[i+1]]);
+	}
+	return (frameArray);
 }
+//
+function detectGesture(frameArray){
+   
+   for (var i=0; i<(frameArray.length-1); i++){
+      if ((frameArray[i][0].vibe === true)||(frameArray[i][0].vibe === true)){
+         //moveon , vibration discounts all gestures
+      }
+      else{
+         if (gesture[0][1].ypos===true){
+            if (Math.abs(frameArray[i][1].y-frameArray[i][0].x)>=gesture[0][1].y){
+               if(Math.abs(frameArray[i+1][1].y-frameArray[i+1][0].x)>=gesture[1][1].y){
+                  console.log("Detection");
+                  return(true);
+               }
+            }
+            else{
+            }             
+         }
+      }      
+   }    
+    return(false);  
+}
+
 //Insert onto screen
-function insertElements() { 
+function insertElements(roll) {
+   if (roll===true){
+      counter++;
+   }
    CounterText.text('Number of Occurances: ' + counter);
    CountScreen.insert(1,CounterText);
    CountScreen.show();
